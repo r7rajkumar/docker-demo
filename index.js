a3612c014713faaf7293d4a46cc3de202409aab1
r7rajkumar <r7rajkumar@gmail.com>
2021-03-30 16:22:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('welcome to the dockertest!');
+  res.send('welcome to the dockertest1!');
 });
 
 var server = app.listen(3000, function () {
